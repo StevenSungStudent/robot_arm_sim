334ef1bfa894be54b6c7992c4949354c1a0dc973
William <w.meijer@student.han.nl>
2024-05-19 17:45:12 +0200

    Update display.launch.py

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -19,13 +20,6 @@ def generate_launch_description():
                                      description='Absolute path to rviz config file')
     ld.add_action(rviz_arg)
 
-    broadcaster_arg = DeclareLaunchArgument(
-            package='robot_arm_sim',
-            executable='robot_arm_sim',
-            name='sim'
-        )
-    ld.add_action(broadcaster_arg)
-
     # This parameter has changed its meaning slightly from previous versions
     ld.add_action(DeclareLaunchArgument(name='model', default_value=default_model_path,
                                         description='Path to robot urdf file relative to urdf_tutorial package'))
@@ -38,5 +32,13 @@ def generate_launch_description():
             'rviz_config': LaunchConfiguration('rvizconfig'),
             'jsp_gui': LaunchConfiguration('gui')}.items()
     ))
+
+
+    ld.add_action(Node(
+    	    package="robot_arm_sim",
+        	executable="robot_arm_sim",
+            parameters=[]
+    	)
+    )
 
     return ld
