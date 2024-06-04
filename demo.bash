eb93efeaca02beb5362a82c0f1ebe6fcbdd15013
William <w.meijer@student.han.nl>
2024-06-04 23:09:08 +0200

    gave the sim a bit more time

--- demo.bash
+++ demo.bash
@@ -3,22 +3,22 @@
 . install/setup.sh
 ros2 launch robot_arm_sim simulation.launch.py &
 
-sleep 3
+sleep 5
 
 ros2 topic pub --once /command std_msgs/msg/String '{data: "#5P1300T2000\r"}'
-sleep 3
+sleep 4
 
 ros2 topic pub --once /command std_msgs/msg/String '{data: "#2P1700T2000\r"}'
-sleep 3
+sleep 4
 
 ros2 topic pub --once /command std_msgs/msg/String '{data: "#1P1300T2000\r"}'
-sleep 3
+sleep 4
 
 ros2 topic pub --once /command std_msgs/msg/String '{data: "#5P1600T2000\r"}'
-sleep 3
+sleep 4
 
 ros2 topic pub --once /command std_msgs/msg/String '{data: "#2P1400T2000\r"}'
-sleep 3
+sleep 4
 
-ros2 topic pub --once /command std_msgs/msg/String '{data: "#5P500T2000\r"}'
-sleep 3
+ros2 topic pub --once /command std_msgs/msg/String '{data: "#5P900T2000\r"}'
+sleep 4
